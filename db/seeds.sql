ab556e738b05e17864a0822bd6083317ff4f26d8
Jake Hart <caboky31@gmail.com>
2019-04-21 15:32:09 -0700

    orm

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+-- In this file, write insert queries to populate the `burgers` table with at least three entries.
+
+INSERT INTO burgers (burger_name, devoured) Values('bacon burger', true);
+INSERT INTO burgers (burger_name, devoured) Values('cheese burger', true);
+INSERT INTO burgers (burger_name, devoured) Values('turkey burger', false);
+
