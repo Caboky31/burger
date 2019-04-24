c5fd14a9846aebf91981a6ef224630f1bed12af1
Jake Hart <caboky31@gmail.com>
2019-04-24 10:00:47 -0700

    it displays now

---
+++ controllers/burgers_controllers.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var burger = require('../models/burger');
+
+router.get('/', function (req, res) {
+    res.redirect('/index');
+  });
+  
+  router.get('/index', function (req, res) {
+    burger.selectAll(function (data) {
+      var object = { burgers: data };
+      res.render('index', object);
+    });
+  });
+
+
+    module.exports = router;
